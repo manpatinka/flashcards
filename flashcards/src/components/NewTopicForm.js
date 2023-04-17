5b7da601aeaf4c578d77f8627842286d68f094ff
Evgenia <evgeniayurkova@gmail.com>
2023-04-17 14:32:37 +0300

    add topicsSlice and its functionality

--- flashcards/src/components/NewTopicForm.js
+++ flashcards/src/components/NewTopicForm.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
+import { addTopic } from "../features/topics/topicsSlice";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name.length === 0) {
       return;
     }
-
+    dispatch(addTopic({
+      id: uuidv4(), 
+      name: name, 
+      icon: icon
+    }))
     // dispatch your add topic action here
     history.push(ROUTES.topicsRoute());
   };
